cc22161c3857c866679f468538db8a59a84663dd
“T3sultan” <incognito.soul011@gmail.com>
2021-10-19 23:03:53 +0600

    redme file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,57 @@
 import React from 'react';
-import { Container, Nav, Navbar,Button } from 'react-bootstrap';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import useAuth from '../../hooks/useAuth';
 import './Header.css'
 
 const Header = () => {
-    const {user,logout} = useAuth();
+    const { user, logout } = useAuth();
     return (
-        
+
         <>
-        <Navbar className="mineContainer" collapseOnSelect expand="lg" sticky="top"  variant="dark">
-            <Container className="container">
-                <Navbar.Brand href="#home" className="hospitalTittle">Specialized  Hospital</Navbar.Brand>
-                <Nav className="">
-                    <Navbar.Toggle />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link as={HashLink} to="/home">Home</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#doctor">Find A Doctor</Nav.Link>
-                        <Nav.Link as={HashLink} to="/home#medicine">Online Medicine</Nav.Link>
-                      {
-                          user?.email?
-                          <Button onClick={logout} variant="outline-light">Logout</Button>
-                          :
+            <Navbar className="mineContainer" collapseOnSelect expand="lg" sticky="top" variant="dark">
+                <Container className="container">
+                    <Navbar.Brand href="#home" className="hospitalTittle">Specialized  Hospital</Navbar.Brand>
+                    <Nav className="">
+                        <Navbar.Toggle />
+                        <Navbar.Collapse className="justify-content-end">
+                            <Nav.Link as={HashLink} to="/home">Home</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#doctor">Find A Doctor</Nav.Link>
+                            <Nav.Link as={HashLink} to="/home#medicine">Online Medicine</Nav.Link>
 
-                          <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                         
+                            {
+                                user?.email ?
+                                    <Button onClick={logout} >Logout</Button>
+                                    :
 
-                      }
+                                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
+
+
+                            }
+                            {
+                                user?.email &&
+                              
+                                    <span style={{ color: "white" }}> {user?.displayName} </span>
+
                                
-                                
-
-                        
-                        <Navbar.Text>
-                            Signed in : <a href="#login">{user.name}</a>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-
-                </Nav>
-            </Container>
-        </Navbar>
-
-    </>
+                            }
 
 
 
-        
+
+                        </Navbar.Collapse>
+
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
+
+
+
+
     );
 };
 

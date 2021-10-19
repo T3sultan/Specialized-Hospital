daa7498c01d7f6ea168dbe8aca4c4c7edba4b436
“T3sultan” <incognito.soul011@gmail.com>
2021-10-19 20:05:28 +0600

    details

--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import './ServiceDetails.css'
 
 const ServiceDetails = () => {
     const { id } = useParams();
+
     const [details, setDetails] = useState([]);
+
     const [individualDetails, setIndividualDetails] = useState({})
 
     useEffect(() => {
-        fetch('serviceDetails.json')
+
+        fetch('/serviceDetails.json')
             .then(res => res.json())
-            .then(data => setDetails(data))
-
-    }, []);
-
+            .then(data => setDetails(data.hospital))
+    }, [])
     useEffect(() => {
         const matchService = details.find(detail => detail.id === id);
 
@@ -21,19 +23,24 @@ const ServiceDetails = () => {
 
     }, [details])
     return (
-        <div className="text-center mt-4 mb-4 details">
-            <Card style={{ width: '28rem', alignItems: 'center' }}>
-                <Card.Img variant="top" className="w-50" src={individualDetails?.imgUrl} />
-                <Card.Body>
-                    <Card.Title>{individualDetails?.name}</Card.Title>
-                    <Card.Text>
-                        {individualDetails?.description}
-                    </Card.Text>
-                   
-                    <Button className="bg-success" variant="primary">See More ...</Button>
-                </Card.Body>
-            </Card>
+        <div className="container4 my-4">
+            <div className="details1">
+
+                <Card className="detailsStyle" style={{ width: '30rem', alignItems: 'center' }}>
+                    <Card.Img variant="top" className="w-100" src={individualDetails?.imgUrl} />
+                    <Card.Body>
+                        <Card.Title>{individualDetails?.name}</Card.Title>
+                        <Card.Text>Description:
+                            {individualDetails?.description}
+                        </Card.Text>
+
+                        <Button className="bg-success" variant="primary">See More ...</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
         </div>
+
     );
 };
 
